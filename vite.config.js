1a8fcc0d8271239f453657ef4d0bcd24d998d15e
Tomas Kostadinov <github@kostadinov.it>
2024-02-28 18:39:09 +0100

    Remove app.css from vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/dispatch/theme.css'],
+            input: ['resources/js/app.js', 'resources/css/filament/dispatch/theme.css'],
             refresh: true,
         }),
     ],
